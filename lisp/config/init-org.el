515a53226c4d3065e2570daedbf0beeaf2f0a086
yuzhou721 <shoper2@163.com>
2022-11-03 23:07:59 +0800

    add org-roam

--- lisp/config/init-org.el
+++ lisp/config/init-org.el
@@ -43,7 +43,8 @@
     (unless org-attach-id-dir
       ;; Centralized attachments directory by default
       (setq-default org-attach-id-dir (expand-file-name ".attach/" org-directory)))
-      (add-to-list 'projectile-globally-ignored-directories org-attach-id-dir))
+    ;; (add-to-list 'projectile-globally-ignored-directories org-attach-id-dir)
+    )
 
 ;; TODO Move to +encrypt flag
 (use-package org-crypt ; built-in
@@ -86,10 +87,56 @@
                               (python     . t)
                               (shell      . t))))
 
+(use-package org-roam
+  :ensure t
+  :custom
+  (org-roam-directory "~/org/roam/")
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n g" . org-roam-ui-open)
+         ("C-c n c" . org-roam-capture)
+         ;; Dailies
+         :map org-mode-map
+         ("C-M-i" . completion-at-point)
+         :map org-roam-dailies-map
+         ("Y" . org-roam-dailies-capture-yesterday)
+         ("T" . org-roam-dailies-capture-tomorrow))
+  :bind-keymap
+  ("C-c n d" . org-roam-dailies-map)
+  :config
+  ;; If you're using a vertical completion framework, you might want a more informative completion interface
+  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (org-roam-db-autosync-mode)
+  ;; If using org-roam-protocol
+  (require 'org-roam-protocol)
+  (require 'org-roam-dailies)
+
+  (setq org-roam-capture-ref-templates
+        '(
+          ("r" "ref" plain
+           "* %?"
+           :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+date: %U\n#+filetags: :web:resource:\n")
+           :unnarrowed t)
+          ("a" "Annotation" plain
+           "${body}\n"
+           :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+date: %U\n#+filetags: :web:resource:\n")
+           ;; :immediate-finish t
+           :unnarrowed t)
+          ))
+  (setq org-roam-dailies-capture-templates
+        `(
+          ("d" "default" entry
+           "* %?"
+           :target (file+head "%<%Y-%m-%d>.org"
+                              "#+title: %<%Y-%m-%d>\n#+filetags: :resource:\n"
+                              ))))
+  )
 
 (use-package evil-org
   :ensure t
   )
+
 
 
 (provide 'init-org)
