287b8438961470747d4c509d162b86e6279f2a6c
yuzhou721 <shoper2@163.com>
2022-11-07 13:22:11 +0800

    自动保存不删除最后空格

--- lisp/config/init-tools.el
+++ lisp/config/init-tools.el
@@ -14,15 +14,16 @@
 
 (use-package auto-save
   :custom
-  (auto-save-idle 60)
+  (auto-save-idle 15)
   :config
   (setq auto-save-silent t)   ; quietly save
-  (setq auto-save-delete-trailing-whitespace t)
+  (setq auto-save-delete-trailing-whitespace nil)
   (setq auto-save-disable-predicates
       '((lambda ()
       (string-suffix-p
       "gpg"
       (file-name-extension (buffer-name)) t))))
+  (auto-save-enable)
   )
 
 (use-package restart-emacs
