7c3d3b3bdeb22aa93309b50035ef0514398bc2b7
dzy <shoper2@163.com>
2022-11-19 17:36:11 +0800

    修改 rime 配置

--- lisp/config/init-rime.el
+++ lisp/config/init-rime.el
@@ -7,6 +7,8 @@
       '(rime-predicate-evil-mode-p
         rime-predicate-after-alphabet-char-p
         rime-predicate-prog-in-code-p))
+  ;;; support shift-l, shift-r, control-l, control-r
+  (setq rime-inline-ascii-trigger 'shift-l)
   )
 
 (provide 'init-rime)
