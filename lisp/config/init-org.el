1fabed18f327875a467d80a2faf9da3c74dddc90
dzy <shoper2@163.com>
2022-11-30 18:12:45 +0800

    org-roam 增加 consult 集成插件

--- lisp/config/init-org.el
+++ lisp/config/init-org.el
@@ -104,7 +104,7 @@
   :custom
   (org-roam-directory "~/org/roam/")
   (org-roam-dailies-directory "~/org/roam/daily/")
-  :bind (("C-c n l" . org-roam-buffer-toggle)
+  :bind (("C-c n t" . org-roam-buffer-toggle)
          ("C-c n f" . org-roam-node-find)
          ("C-c n i" . org-roam-node-insert)
          ("C-c n g" . org-roam-ui-open)
@@ -178,5 +178,33 @@
 (use-package org-modern
   :hook (org-mode . org-modern-mode)
   )
+
+(use-package consult-org-roam
+   :ensure nil
+   :after org-roam
+   :init
+   (require 'consult-org-roam)
+   ;; Activate the minor mode
+   (consult-org-roam-mode 1)
+   :custom
+   ;; Use `ripgrep' for searching with `consult-org-roam-search'
+   (consult-org-roam-grep-func #'consult-ripgrep)
+   ;; Configure a custom narrow key for `consult-buffer'
+   (consult-org-roam-buffer-narrow-key ?r)
+   ;; Display org-roam buffers right after non-org-roam buffers
+   ;; in consult-buffer (and not down at the bottom)
+   (consult-org-roam-buffer-after-buffers t)
+   :config
+   ;; Eventually suppress previewing for certain functions
+   (consult-customize
+    consult-org-roam-forward-links
+    :preview-key (kbd "M-."))
+   :bind
+   ;; Define some convenient keybindings as an addition
+   ("C-c n F" . consult-org-roam-file-find)
+   ("C-c n b" . consult-org-roam-backlinks)
+   ("C-c n l" . consult-org-roam-forward-links)
+   ("C-c n s" . consult-org-roam-search))
+
 (provide 'init-org)
 ;;; init-org.el ends here
