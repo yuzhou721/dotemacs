eb3f19f2503ec784ec08f445290fd113bb65d5e2
yuzhou <shoper2@163.com>
2023-09-11 18:26:45 +0800

    fixbug cape

--- lisp/config/init-completion.el
+++ lisp/config/init-completion.el
@@ -1,3 +1,4 @@
+
 (use-package corfu
 ;; Optional customizations
   :init
@@ -24,26 +25,7 @@
     (setq cape-dabbrev-min-length 3)
     ;; Add `completion-at-point-functions', used by `completion-at-point'.
     (add-to-list 'completion-at-point-functions #'cape-file)
-    (add-to-list 'completion-at-point-functions #'cape-tex)
-    (add-to-list 'completion-at-point-functions #'cape-dabbrev)
-    (add-to-list 'completion-at-point-functions #'cape-keyword)
-   ;; Bind dedicated completion commands
-   :bind (("C-c p p" . completion-at-point) ;; capf
-           ("C-c p t" . complete-tag)        ;; etags
-           ("C-c p d" . cape-dabbrev)        ;; or dabbrev-completion
-           ("C-c p f" . cape-file)
-           ("C-c p k" . cape-keyword)
-           ("C-c p s" . cape-symbol)
-           ("C-c p a" . cape-abbrev)
-           ("C-c p i" . cape-ispell)
-           ("C-c p l" . cape-line)
-           ("C-c p w" . cape-dict)
-           ("C-c p \\" . cape-tex)
-           ("C-c p _" . cape-tex)
-           ("C-c p ^" . cape-tex)
-           ("C-c p &" . cape-sgml)
-           ("C-c p r" . cape-rfc1345))
-  )
+    (add-to-list 'completion-at-point-functions #'cape-dabbrev))
 
 ;; Enable vertico
 (use-package vertico
