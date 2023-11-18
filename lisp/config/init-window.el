0dd17c883dca22357d258160d3f8a9316645fee2
yuzhou <shoper2@163.com>
2023-11-19 00:42:33 +0800

    修复快捷键冲突问题

--- lisp/config/init-window.el
+++ lisp/config/init-window.el
@@ -37,105 +37,108 @@ Main data structure of the dispatcher with the form:
   :defines opper-echo-dispatch-actions
   :commands popper-group-by-directory
   :bind (:map popper-mode-map
-	      ("s-`" . popper-toggle-latest)
-	      ("s-o"   . popper-cycle)
-	      ("M-`" . popper-toggle-type))
+			  ("C-`" . popper-toggle-latest)
+			  ;; ("s-o" . popper-cycle)
+			  ;; ("M-`" . popper-toggle-type)
+			  )
   :hook (emacs-startup . popper-mode)
   :init
   (setq popper-reference-buffers
-	'("\\*Messages\\*"
-	  "Output\\*$" "\\*Pp Eval Output\\*$"
-	  "\\*Compile-Log\\*"
-	  "\\*Completions\\*"
-	  "\\*Warnings\\*"
-	  "\\*Flymake diagnostics.*\\*"
-	  "\\*Async Shell Command\\*"
-	  "\\*Apropos\\*"
-	  "\\*Backtrace\\*"
-	  "\\*prodigy\\*"
-	  "\\*Calendar\\*"
-	  "\\*Embark Actions\\*"
-	  "\\*Finder\\*"
-	  "\\*Kill Ring\\*"
-	  "\\*Embark Export:.*\\*"
-	  "\\*Edit Annotation.*\\*"
-	  "\\*Flutter\\*"
-	  "\\*lsp-bridge\\*"
-	  bookmark-bmenu-mode
-	  lsp-bridge-ref-mode
-	  comint-mode
-	  compilation-mode
-	  help-mode helpful-mode
-	  tabulated-list-mode
-	  Buffer-menu-mode
-	  occur-mode
-	  gnus-article-mode devdocs-mode
-	  grep-mode occur-mode rg-mode deadgrep-mode ag-mode pt-mode
-	  ivy-occur-mode ivy-occur-grep-mode
-	  process-menu-mode list-environment-mode cargo-process-mode
-	  youdao-dictionary-mode osx-dictionary-mode fanyi-mode
+		'("\\*Messages\\*"
+		  "Output\\*$" "\\*Pp Eval Output\\*$"
+		  "\\*Compile-Log\\*"
+		  "\\*Completions\\*"
+		  "\\*Warnings\\*"
+		  "\\*Flymake diagnostics.*\\*"
+		  "\\*Async Shell Command\\*"
+		  "\\*Apropos\\*"
+		  "\\*Backtrace\\*"
+		  "\\*prodigy\\*"
+		  "\\*Calendar\\*"
+		  "\\*Embark Actions\\*"
+		  "\\*Finder\\*"
+		  "\\*Kill Ring\\*"
+		  "\\*Embark Export:.*\\*"
+		  "\\*Edit Annotation.*\\*"
+		  "\\*Flutter\\*"
+		  "\\*lsp-bridge\\*"
+		  bookmark-bmenu-mode
+		  lsp-bridge-ref-mode
+		  comint-mode
+		  compilation-mode
+		  help-mode helpful-mode
+		  tabulated-list-mode
+		  Buffer-menu-mode
+		  occur-mode
+		  gnus-article-mode devdocs-mode
+		  grep-mode occur-mode rg-mode deadgrep-mode ag-mode pt-mode
+		  ivy-occur-mode ivy-occur-grep-mode
+		  process-menu-mode list-environment-mode cargo-process-mode
+		  youdao-dictionary-mode osx-dictionary-mode fanyi-mode
 
-	  "^\\*eshell.*\\*.*$" eshell-mode
-	  "^\\*shell.*\\*.*$"  shell-mode
-	  "^\\*terminal.*\\*.*$" term-mode
-	  "^\\*vterm.*\\*.*$"  vterm-mode
+		  "^\\*eshell.*\\*.*$" eshell-mode
+		  "^\\*shell.*\\*.*$" shell-mode
+		  "^\\*terminal.*\\*.*$" term-mode
+		  "^\\*vterm.*\\*.*$" vterm-mode
 
-	  "\\*DAP Templates\\*$" dap-server-log-mode
-	  "\\*ELP Profiling Restuls\\*" profiler-report-mode
-	  "\\*Flycheck errors\\*$" " \\*Flycheck checker\\*$"
-	  "\\*Paradox Report\\*$" "\\*package update results\\*$" "\\*Package-Lint\\*$"
-	  "\\*[Wo]*Man.*\\*$"
-	  "\\*ert\\*$" overseer-buffer-mode
-	  "\\*gud-debug\\*$"
-	  "\\*lsp-help\\*$" "\\*lsp session\\*$"
-	  "\\*quickrun\\*$"
-	  "\\*tldr\\*$"
-	  "\\*vc-.*\\*$"
-	  "\\*eldoc\\*"
-	  "\\*General Keybindings\\*"
-	  "^\\*elfeed-entry\\*$"
-	  "^\\*macro expansion\\**"
+		  "\\*DAP Templates\\*$" dap-server-log-mode
+		  "\\*ELP Profiling Restuls\\*" profiler-report-mode
+		  "\\*Flycheck errors\\*$" " \\*Flycheck checker\\*$"
+		  "\\*Paradox Report\\*$" "\\*package update results\\*$" "\\*Package-Lint\\*$"
+		  "\\*[Wo]*Man.*\\*$"
+		  "\\*ert\\*$" overseer-buffer-mode
+		  "\\*gud-debug\\*$"
+		  "\\*lsp-help\\*$" "\\*lsp session\\*$"
+		  "\\*quickrun\\*$"
+		  "\\*tldr\\*$"
+		  "\\*vc-.*\\*$"
+		  "\\*eldoc\\*"
+		  "\\*General Keybindings\\*"
+		  "^\\*elfeed-entry\\*$"
+		  "^\\*macro expansion\\**"
 
-	  "\\*Agenda Commands\\*" "\\*Org Select\\*" "\\*Capture\\*" "^CAPTURE-.*\\.org*"
-	  "\\*Gofmt Errors\\*$" "\\*Go Test\\*$" godoc-mode
-	  "\\*docker-containers\\*" "\\*docker-images\\*" "\\*docker-networks\\*" "\\*docker-volumes\\*"
-	  "\\*prolog\\*" inferior-python-mode inf-ruby-mode swift-repl-mode
-	  "\\*rustfmt\\*$" rustic-compilation-mode rustic-cargo-clippy-mode
-	  rustic-cargo-outdated-mode rustic-cargo-test-moed))
+		  "\\*Agenda Commands\\*" "\\*Org Select\\*" "\\*Capture\\*" "^CAPTURE-.*\\.org*"
+		  "\\*Gofmt Errors\\*$" "\\*Go Test\\*$" godoc-mode
+		  "\\*docker-containers\\*" "\\*docker-images\\*" "\\*docker-networks\\*" "\\*docker-volumes\\*"
+		  "\\*prolog\\*" inferior-python-mode inf-ruby-mode swift-repl-mode
+		  "\\*rustfmt\\*$" rustic-compilation-mode rustic-cargo-clippy-mode
+		  rustic-cargo-outdated-mode rustic-cargo-test-moed
+		  cider-repl-mode))
 
   (when (display-grayscale-p)
-    (setq popper-mode-line
-	  '(:eval
-	    (concat
-	     (propertize " " 'face 'mode-line-emphasis)
-	     (all-the-icons-octicon "pin" :height 0.9 :v-adjust 0.0 :face 'mode-line-emphasis)
-	     (propertize " " 'face 'mode-line-emphasis)))))
+	(setq popper-mode-line
+		  '(:eval
+			(concat
+			 (propertize " " 'face 'mode-line-emphasis)
+			 (all-the-icons-octicon "pin" :height 0.9 :v-adjust 0.0 :face 'mode-line-emphasis)
+			 (propertize " " 'face 'mode-line-emphasis)))))
 
   (setq popper-echo-dispatch-actions t)
   (setq popper-group-function nil)
+  (setq popper-group-function #'popper-group-by-project) ; project.el projects
   :config
   (require 'popper-echo)
   (popper-echo-mode 1)
 
   (with-no-warnings
-    (defun my-popper-fit-window-height (win)
-      "Determine the height of popup window WIN by fitting it to the buffer's content."
-      (fit-window-to-buffer
-       win
-       (floor (frame-height) 3)
-       (floor (frame-height) 3)))
-    (setq popper-window-height #'my-popper-fit-window-height)
+	(defun my-popper-fit-window-height (win)
+	  "Determine the height of popup window WIN by fitting it to the buffer's content."
+	  (fit-window-to-buffer
+	   win
+	   (floor (frame-height) 3)
+	   (floor (frame-height) 3)))
+	(setq popper-window-height #'my-popper-fit-window-height)
 
-    (defun popper-close-window-hack (&rest _)
-      "Close popper window via `C-g'."
-      ;; `C-g' can deactivate region
-      (when (and (called-interactively-p 'interactive)
-		 (not (region-active-p))
-		 popper-open-popup-alist)
-	(let ((window (caar popper-open-popup-alist)))
-	  (when (window-live-p window)
-	    (delete-window window)))))
-    (advice-add #'keyboard-quit :before #'popper-close-window-hack)))
+	(defun popper-close-window-hack (&rest _)
+	  "Close popper window via `C-g'."
+	  ;; `C-g' can deactivate region
+	  (when (and (called-interactively-p 'interactive)
+				 (not (region-active-p))
+				 popper-open-popup-alist)
+		(let ((window (caar popper-open-popup-alist)))
+		  (when (window-live-p window)
+			(delete-window window)))))
+	(advice-add #'keyboard-quit :before #'popper-close-window-hack)))
 
 (use-package buffer-move
   :ensure t)
