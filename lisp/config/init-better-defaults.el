3105d52298581b2f815a08f53873c0bb1d81f7a3
yuzhou <shoper2@163.com>
2023-11-19 00:42:01 +0800

    add smartparent package

--- lisp/config/init-better-defaults.el
+++ lisp/config/init-better-defaults.el
@@ -293,5 +293,16 @@ Else, call `comment-or-uncomment-region' on the current line."
 	ediff-split-window-function #'split-window-horizontally
 	ediff-window-setup-function #'ediff-setup-windows-plain))
 
+(use-package smartparens
+  :ensure t
+  :init
+  (smartparens-global-mode t)
+  (sp-local-pair 'emacs-lisp-mode "'" nil :actions nil)
+  (sp-local-pair 'emacs-lisp-mode "`" nil :actions nil)
+  (sp-local-pair 'lisp-interaction-mode "'" nil :actions nil)
+  :config
+    (sp-with-modes
+        '(c++-mode objc-mode c-mode)
+      (sp-local-pair "{" nil :post-handlers '(:add ("||\n[i]" "RET")))))
 
 (provide 'init-better-defaults)
